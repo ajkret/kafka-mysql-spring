0baebc1d7dd782541b08c5a6d713427b970e7a97
Adriano Kretschmer <a.kretschmer@cinq.com.br>
2020-03-06 15:46:36 -0300

    Fixed producer, finally

--- producer/src/main/java/com/cinq/cloudstraeamdemo/converter/ConvertCity.java
+++ producer/src/main/java/com/cinq/cloudstraeamdemo/converter/ConvertCity.java
@@ -17,14 +17,14 @@ public class ConvertCity {
     City property = new City();
     property.setCity(fields[0]);
     property.setState(fields[1]);
-    property.setResidents(Integer.valueOf(fields[3]));
-    property.setForeigners(Integer.valueOf(fields[5]));
-    property.setDomesticUnits(Integer.valueOf(fields[6]));
-    property.setHdiRanking(Double.valueOf(fields[19]));
-    property.setLongitude(Double.valueOf(fields[23]));
-    property.setLatitude(Double.valueOf(fields[24]));
-    property.setGdp(Double.valueOf(fields[39]));
-    property.setPopulation(Integer.valueOf(fields[31]));
+    property.setResidents(fields[3].isEmpty()?0:Integer.valueOf(fields[3]));
+    property.setForeigners(fields[5].isEmpty()?0:Integer.valueOf(fields[5]));
+    property.setDomesticUnits(fields[6].isEmpty()?0:Integer.valueOf(fields[6]));
+    property.setHdiRanking(fields[19].isEmpty()?0.0:Double.valueOf(fields[19]));
+    property.setLongitude(fields[23].isEmpty()?0.0:Double.valueOf(fields[23]));
+    property.setLatitude(fields[24].isEmpty()?0.0:Double.valueOf(fields[24]));
+    property.setGdp(fields[39].isEmpty()?0.0:Double.valueOf(fields[39]));
+    property.setPopulation(fields[31].isEmpty()?0:Integer.valueOf(fields[31]));
 
     return property;
   }

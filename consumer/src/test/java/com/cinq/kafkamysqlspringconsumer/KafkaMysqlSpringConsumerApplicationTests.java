56c2381fb8ffed3d0b165546d6b87cf1ee032dc8
Adriano Kretschmer <a.kretschmer@cinq.com.br>
2020-03-09 16:35:25 -0300

    Adding Consumer

---
+++ consumer/src/test/java/com/cinq/kafkamysqlspringconsumer/KafkaMysqlSpringConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinq.kafkamysqlspringconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMysqlSpringConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
